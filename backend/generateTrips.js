8d39483196bcc2cb76bea6299ca6975af87927f7
Julien BARRA <Julien.barra89100@gmail.coml>
2024-04-16 10:22:51 +0200

    ok

--- backend/generateTrips.js
+++ backend/generateTrips.js
@@ -1,11 +1,11 @@
 // This program generate trips in trips.json file. Feel free to modify or improve it!
 
-const moment = require('moment');
-const momentRandom = require('moment-random');
-const fs = require('fs');
+const moment = require("moment");
+const momentRandom = require("moment-random");
+const fs = require("fs");
 
 const NB_TRIPS = 2000;
-const CITIES = ['Paris', 'Lyon', 'Marseille', 'Bruxelles'];
+const CITIES = ["Paris", "Lyon", "Marseille", "Bruxelles"];
 const MIN_PRICE = 25;
 const MAX_PRICE = 150;
 
@@ -21,15 +21,22 @@ function randomNumber(min, max) {
 const trips = [];
 for (let i = 0; i < NB_TRIPS; i++) {
   const [departure, arrival] = randomElement(CITIES, 2);
-  const date = momentRandom(moment().add(2, 'weeks'), moment());
-  trips.push({ departure, arrival, date: { $date: date.toDate() }, price: randomNumber(MIN_PRICE, MAX_PRICE) });
+  const date = momentRandom(moment().add(2, "weeks"), moment());
+  trips.push({
+    departure,
+    arrival,
+    date: { $date: date.toDate() },
+    price: randomNumber(MIN_PRICE, MAX_PRICE),
+  });
 }
 
 const sortedTrips = trips.sort((a, b) => a.date.$date - b.date.$date);
-fs.writeFile('./trips.json', JSON.stringify(sortedTrips), (err) => {
+fs.writeFile("./trips.json", JSON.stringify(sortedTrips), (err) => {
   if (err) {
     console.error(err);
   } else {
-    console.log(`${NB_TRIPS} trips have been generated in trips.json file. Happy hackathon!`);
+    console.log(
+      `${NB_TRIPS} trips have been generated in trips.json file. Happy hackathon!`
+    );
   }
 });
