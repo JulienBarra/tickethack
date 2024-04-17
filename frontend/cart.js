98632a29809dead8e8edd6a3771a8d48b1940119
Julien BARRA <Julien.barra89100@gmail.coml>
2024-04-17 17:51:10 +0200

    ddd

--- frontend/cart.js
+++ frontend/cart.js
@@ -2,20 +2,25 @@ fetch("http://localhost:3000/carts")
   .then((response) => response.json())
   .then((result) => {
     console.log(result);
+
     document.querySelector("#cart-default").style.display = "none";
-    document.querySelector(".card").innerHTML += `
+
+    const stringDate = result.trips[0].trip.date;
+    const formatDate = stringDate.slice(11, 16);
+
+    document.querySelector("#card-one").innerHTML += `
     <div id="trip-cart">
             <p id="mycart">My cart</p>
             <div class="result-trip">
               <p>
-                <span id="departure">Paris</span> >
-                <span id="arrival">Lyon</span> <span id="hour">16:23</span>
-                <span id="price">126</span>€
+                <span id="departure">${result.trips[0].trip.departure}</span> >
+                <span id="arrival">${result.trips[0].trip.arrival}</span> <span id="hour">${formatDate}</span>
+                <span id="price">${result.trips[0].trip.price}</span>€
               </p>
               <button type="button" id="btn-x">X</button>
             </div>
             <div class="total-purchase">
-              <p>Total:<span id="price-purchase">103</span>€</p>
+              <p>Total:<span id="price-purchase">${result.trips[0].trip.price}</span>€</p>
               <button type="button" id="btn-purchase">Purchase</button>
             </div>
           </div>`;
