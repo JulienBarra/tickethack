5d2fc8b0faab5facfb6030ffe9750fd4e3408e6b
Julien BARRA <Julien.barra89100@gmail.coml>
2024-04-17 09:13:16 +0200

    liste truc à faire

--- frontend/script.js
+++ frontend/script.js
@@ -15,3 +15,32 @@ document.querySelector("#btn-search").addEventListener("click", function () {
       console.log(result.trips);
     });
 });
+
+//FRONTEND
+
+// bouton search - Home
+// Si la recherche ne présente aucun résultats: afficher "No trip found" + image loupe croix
+// Si la recherche présente plusieurs résultat: afficher les voyages a droite
+
+// bouton book - Home
+// lors du click : - le click renvoie sur la page My cart
+//                 - ajoute le voyages selectionner dans le panier
+//                 - le cout total du panier change (++)
+//                 - le logo du site doit renvoyer sur la page d'accueil
+
+// bouton delete - My cart
+// lors du click : - supprime le voyage dans le panier +
+//                 -  MAJ du cout total du panier (--)
+
+// bouton Purchase - My cart
+// lors du click : - renvoie sur la page My booking
+//                 - affiche le résumé des voyages validé dans le panier
+//                 - affiche pour chaque voyage le temps restant avant le départ
+//                 - vide le panier
+
+// BACKEND
+
+// créer une route carts/addCart : - permet d'ajouter un voyage dans la collection carts
+//                                 - (voir pour donner un ID à la commande ?)
+
+// créer une route carts/deleteCart : - permet de supprimer un voyage de la collection carts
